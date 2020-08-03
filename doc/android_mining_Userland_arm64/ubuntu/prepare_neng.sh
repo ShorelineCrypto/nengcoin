3f7aefab17065fa84a6ba5da42720cbdfaabc60d
ShorelineCrypto <admin@shorelinecrypto.com>
2020-08-02 22:49:25 -0400

    Consolidate into one command shell

--- doc/android_mining_Userland_arm64/ubuntu/prepare_neng.sh
+++ doc/android_mining_Userland_arm64/ubuntu/prepare_neng.sh
@@ -3,18 +3,24 @@ sudo apt-get -y install build-essential g++ libtool autotools-dev automake pkg-c
 sudo apt-get -y install software-properties-common
  
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
- 
 sudo apt-get -y update
- 
 sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
  
-sudo apt-get -y install libzmq3-dev libbz2-dev 
- 
-## sometimes zlib will generate error, reinstall this
-sudo apt-get -y install --reinstall zlib1g
- 
-sudo apt-get -y install libminiupnpc-dev
+sudo apt-get -y install libzmq3-dev libbz2-dev zlib1g libminiupnpc-dev
+sudo apt-get -y install libqt4-dev libprotobuf-dev protobuf-compiler
 
 sudo apt -y install git
 sudo apt -y install python-pip
 sudo pip install python-bitcoinrpc
+
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0/ubuntu16.04_boost1.58_arm64.tgz
+tar xvfz ubuntu16.04_boost1.58_arm64.tgz
+sudo rm -rf /opt/boost1.58
+sudo mv boost1.58  /opt/
+sudo mv boost1.58.conf  /etc/ld.so.conf.d/
+sudo ldconfig
+
+git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
+wget  https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0/newenglandcoin_v1.4.0_u16_arm64.tgz
+tar xvfz newenglandcoin_v1.4.0_u16_arm64.tgz
+
--- doc/android_mining_Userland_arm64/ubuntu/prepare_nengX.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update
-sudo apt-get -y install libqt4-dev libprotobuf-dev protobuf-compiler
-
