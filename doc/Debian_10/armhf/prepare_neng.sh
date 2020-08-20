331219653f87e6749ab067e925120e87049bffc6
ShorelineCrypto <admin@shorelinecrypto.com>
2020-08-20 02:17:10 -0400

    added armhf for Debian 10

---
+++ doc/Debian_10/armhf/prepare_neng.sh
@@ -0,0 +1,26 @@
+sudo apt-get update
+sudo apt-get -y install wget curl
+sudo apt-get -y install build-essential 
+sudo apt-get -y install g++ libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils 
+sudo apt-get -y install libboost-all-dev
+
+sudo apt-get -y install software-properties-common
+sudo apt-get update
+
+ 
+sudo apt-get -y install libzmq3-dev libbz2-dev zlib1g 
+sudo apt-get -y install libprotobuf-dev protobuf-compiler
+sudo apt-get -y install openssl
+
+sudo apt-get -y install qt4-default
+
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-setuptools
+sudo pip install python-bitcoinrpc
+
+sudo apt-get -y install git
+#git clone https://github.com/ShorelineCrypto/NewEnglandCoin.git
+git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
+
+
+
