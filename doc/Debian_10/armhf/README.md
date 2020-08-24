37d29e2ee6fe0ad6fb73d700bce51335a377ed16
ShorelineCrypto <admin@shorelinecrypto.com>
2020-08-23 20:53:40 -0400

    Debian10 binary switched from deb8 to u16

--- doc/Debian_10/armhf/README.md
+++ doc/Debian_10/armhf/README.md
@@ -1,6 +1,6 @@
-#  Debian 10 (buster) on armhf 
+# Debian 10 (buster) on armhf
 
-This folder describe a binary support from v1.4.0.5 NENG core release on  Debian 9 (stretch) / 10 (buster).
+This folder describe a binary support from v1.4.0.5 NENG core release on Debian 9 (stretch) / 10 (buster).
 
 ## Step 1: Install library files
 Please run below shell script in terminal for installing dependencies and library files:
@@ -10,6 +10,6 @@ Please run below shell script in terminal for installing dependencies and librar
 
 ## Step 2: download the binary wallet and enjoy:
 ```
-  wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0.5/newenglandcoin_v1.4.0.5_debian8_armhf.tgz
-  tar xvfz newenglandcoin_v1.4.0.5_debian8_armhf.tgz 
+  wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0.5/newenglandcoin_v1.4.0.5_ubuntu16_armhf.tgz
+  tar xvfz newenglandcoin_v1.4.0.5_ubuntu16_armhf.tgz
 ```
--- doc/Debian_10/armhf/prepare_neng.sh
+++ doc/Debian_10/armhf/prepare_neng.sh
@@ -23,4 +23,29 @@ sudo apt-get -y install git
 git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
 
 
+# Berkeley DB: http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+# This can not be compiled and used for NENG wallet
+# we obtained binaries from Ubuntu 18.04 and bundled in one folder
+
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0.5/ubuntu18_armhf_db-4.8.30.NC.tgz
+tar xvfz ubuntu18_armhf_db-4.8.30.NC.tgz
+sudo mv db-4.8.30.NC  /opt/
+sudo mv bitcoin.conf /etc/ld.so.conf.d/
+
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0.5/ubuntu18_armhf_miniupnpc.tgz
+tar xvfz ubuntu18_armhf_miniupnpc.tgz
+sudo mv miniupnpc /opt/
+sudo mv miniupnpc.conf /etc/ld.so.conf.d/ 
+
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0.5/ubuntu16_armhf_openssl1.0.tgz
+tar xvfz ubuntu16_armhf_openssl1.0.tgz
+sudo mv openssl  /opt/
+sudo mv openssl.conf /etc/ld.so.conf.d/ 
+
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0.5/ubuntu16_armhf_boost1.58.tgz 
+tar xvfz ubuntu16_armhf_boost1.58.tgz
+sudo mv boost1.58  /opt/
+sudo mv boost1.58.conf  /etc/ld.so.conf.d/
+## link all libary files
+sudo ldconfig
 
