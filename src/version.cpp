87dcfd351fa67425c4271304b748ed95bfc0a61d
ShorelineCrypto <admin@shorelinecrypto.com>
2020-11-06 21:42:46 -0500

    version tag client change randomSpike-v1.4.x

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-beta"
+#define CLIENT_VERSION_SUFFIX   "-v1.4.x"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
@@ -43,7 +43,7 @@ const std::string CLIENT_NAME("Satoshi");
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-randomSpike"
 
 #ifndef BUILD_DESC
 #    ifdef GIT_COMMIT_ID
