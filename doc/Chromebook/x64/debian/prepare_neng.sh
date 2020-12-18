2092d48122b166b3863c4cbf5864e2300280eb74
ShorelineCrypto <admin@shorelinecrypto.com>
2020-12-17 20:29:19 -0500

    update chromebook debian x64

--- doc/Chromebook/x64/debian/prepare_neng.sh
+++ doc/Chromebook/x64/debian/prepare_neng.sh
@@ -19,7 +19,6 @@ sudo apt-get -y install python-setuptools
 sudo pip install python-bitcoinrpc
 
 sudo apt-get -y install git
-#git clone https://github.com/ShorelineCrypto/NengCoin.git
 git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
 
 
@@ -27,29 +26,33 @@ git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
 # This can not be compiled and used for NENG wallet
 # we obtained binaries from Ubuntu 18.04 and bundled in one folder
 
-wget https://github.com/ShorelineCrypto/NengCoin/releases/download/v1.4.0/ubuntu20.04_x86_64_db-4.8.30.NC.tgz
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0/ubuntu20.04_x86_64_db-4.8.30.NC.tgz
 tar xvfz ubuntu20.04_x86_64_db-4.8.30.NC.tgz 
+sudo rm -rf /opt/db-4.8.30.NC
 sudo mv db-4.8.30.NC  /opt/
 sudo mv bitcoin.conf /etc/ld.so.conf.d/
 
-wget https://github.com/ShorelineCrypto/NengCoin/releases/download/v1.4.0/ubuntu20.04_x86_64_miniupnpc.tgz
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0/ubuntu20.04_x86_64_miniupnpc.tgz
 tar xvfz ubuntu20.04_x86_64_miniupnpc.tgz
+sudo rm -rf /opt/miniupnpc
 sudo mv miniupnpc /opt/
 sudo mv miniupnpc.conf /etc/ld.so.conf.d/ 
 
-wget https://github.com/ShorelineCrypto/NengCoin/releases/download/v1.4.0/ubuntu20.04_x86_64_openssl-1.0.0.tgz
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0/ubuntu20.04_x86_64_openssl-1.0.0.tgz
 tar xvfz ubuntu20.04_x86_64_openssl-1.0.0.tgz 
+sudo rm -rf /opt/openssl
 sudo mv openssl  /opt/
 sudo mv openssl.conf /etc/ld.so.conf.d/ 
 
 ## reuse prior compiled boost libary for Ubuntu 18.04
-wget https://github.com/ShorelineCrypto/NengCoin/releases/download/v1.2.1.1/ubuntu18.04_boost1.58.tgz
+wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.2.1.1/ubuntu18.04_boost1.58.tgz
 tar xvfz ubuntu18.04_boost1.58.tgz
+sudo rm -rf /opt/boost1.58
 sudo mv boost1.58  /opt/
 sudo mv boost1.58.conf  /etc/ld.so.conf.d/
 ## link all libary files
 sudo ldconfig
 
 
-wget https://github.com/ShorelineCrypto/NengCoin/releases/download/v1.4.1.1/nengcoin_v1.4.1.1_ubuntu16.04.tgz
-tar xvfz nengcoin_v1.4.1.1_ubuntu16.04.tgz
+wget https://github.com/ShorelineCrypto/nengcoin/releases/download/v1.5.0.0/nengcoin_v1.5.0.0_ubuntu16.04.tgz
+tar xvfz nengcoin_v1.5.0.0_ubuntu16.04.tgz
