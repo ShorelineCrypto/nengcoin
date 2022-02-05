ca916f9a8452c7d6b377667190bb93e57b69d775
ShorelineCrypto <admin@shorelinecrypto.com>
2022-02-04 22:10:19 -0500

    update scripts on android

--- doc/Android_Userland_App/arm64/prepare_neng.sh
+++ doc/Android_Userland_App/arm64/prepare_neng.sh
@@ -1,26 +1,19 @@
-sudo apt-get -y install build-essential g++ libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils  libboost-all-dev
 
 sudo apt-get -y install software-properties-common
  
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get -y update
-sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
  
-sudo apt-get -y install libzmq3-dev libbz2-dev zlib1g libminiupnpc-dev
-sudo apt-get -y install libqt4-dev libprotobuf-dev protobuf-compiler
 
-sudo apt -y install git
-sudo apt -y install python-pip
-sudo pip install python-bitcoinrpc
+sudo apt -y install curl git
 
-wget https://github.com/ShorelineCrypto/NewEnglandCoin/releases/download/v1.4.0/ubuntu16.04_boost1.58_arm64.tgz
-tar xvfz ubuntu16.04_boost1.58_arm64.tgz
-sudo rm -rf /opt/boost1.58
-sudo mv boost1.58  /opt/
-sudo mv boost1.58.conf  /etc/ld.so.conf.d/
-sudo ldconfig
+sudo apt -y install python
+sudo apt -y install python2
+
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+sudo  python2 get-pip.py
 
 git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
-wget  https://github.com/ShorelineCrypto/nengcoin/releases/download/v1.9.1.2/nengcoin_v1.9.1.2_u16_arm64.tgz
-tar xvfz nengcoin_v1.9.1.2_u16_arm64.tgz 
+
+wget  https://github.com/ShorelineCrypto/nengcoin/releases/download/v2.0.0/nengcoin_2.0.0_arm64_linux-gnu.tgz
+tar xvfz nengcoin_2.0.0_arm64_linux-gnu.tgz 
 
