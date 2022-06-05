f2eef8868d289e2e0a600c3b75edfbaf8162b69e
ShorelineCrypto <admin@shorelinecrypto.com>
2022-06-04 22:03:23 -0400

    update checkpoint minChainWork

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -98,7 +98,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1751380513; // July 1, 2025 
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000012ad0d7c04752dd");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000147f60c54c7d394");
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -144,9 +144,10 @@ public:
 			( 3244063, uint256S("0xf0f3a907f7f13d549647049f2263a47f5c8a3c938aa9937b8f8d2d880130cbe3"))
 			( 3244189, uint256S("0x29a936ffb34341d4fee4d29bb59890442ddb4893eadd5a8f1c8f7a7e6b2ca583"))
 			( 3251189, uint256S("0x5956c5280370bb2812314d38a2de987f9fa33aa628276d97344761e244b1a3bf"))
-			( 3539846, uint256S("0x75ca0749a1975ce38f17a2ccf919c2478e8bcd1fc6c107642cff3a3e63878502")),
-			1645574876, // * UNIX timestamp of last checkpoint block
-			4512019,   // * total number of transactions between genesis and last checkpoint
+			( 3539846, uint256S("0x75ca0749a1975ce38f17a2ccf919c2478e8bcd1fc6c107642cff3a3e63878502"))
+            ( 3744303, uint256S("0x3e012d140a2604b7852a78790d94de23c1f834a283c444da4f1db02925e37078")),
+			1654393137, // * UNIX timestamp of last checkpoint block
+			4728419,   // * total number of transactions between genesis and last checkpoint
 			//   (the tx=... number in the SetBestChain debug.log lines)
 			100.0     // * estimated number of transactions per day after checkpoint
 	};
